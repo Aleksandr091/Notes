052896064d5ad7a08bebb7ef1d4c47cada67cb3c
Александр <chist675@gmail.com>
2022-02-19 22:18:37 +0300

    Д\з к 7-му уроку,создал вложенный фрагмент

--- app/src/main/java/ru/chistov/notes/DescriptionChildFragment.java
+++ app/src/main/java/ru/chistov/notes/DescriptionChildFragment.java
@@ -4,24 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import java.util.List;
 
 
-public class DescriptionFragment extends Fragment {
+public class DescriptionChildFragment extends Fragment {
 
     public static final String ARG_NOTE = "Note";
     private Note note;
 
-    public static DescriptionFragment newInstance(Note note) {
-        DescriptionFragment fragment = new DescriptionFragment();
+    public static DescriptionChildFragment newInstance(Note note) {
+        DescriptionChildFragment fragment = new DescriptionChildFragment();
         Bundle bundle=new Bundle();
         bundle.putParcelable(ARG_NOTE,note);
         fragment.setArguments(bundle);
@@ -33,25 +31,21 @@ public class DescriptionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_description_notes, container, false);
+        return inflater.inflate(R.layout.fragment_description_notes_child, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         note = getArguments().getParcelable(ARG_NOTE);
+        EditText ed = view.findViewById(R.id.ed_txt_dn);
+        String[] description = getResources().getStringArray(R.array.description);
+        ed.setText(description[note.getIndex()]);
 
-        getChildFragmentManager().beginTransaction().replace(R.id.container_child,DescriptionChildFragment.newInstance(note)).addToBackStack("").commit();
-
-        view.findViewById(R.id.btn_rem).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                List<Fragment> list= requireActivity().getSupportFragmentManager().getFragments();
-                for (Fragment fr:list) {
-                    if(fr instanceof DescriptionFragment){
-                        requireActivity().getSupportFragmentManager().beginTransaction().remove(fr).commit();
-                    }
-                }
+                getParentFragmentManager().popBackStack();
             }
         });
 
